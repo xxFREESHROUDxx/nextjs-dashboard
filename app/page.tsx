87014c7bde2539fa627c20844a19f6a5c73d2113
baibhav <baibhav@bookmundi.com>
2023-12-20 14:10:29 +0545

    chore: naming fixed for page component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { lustina } from './ui/fonts';
+import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 
 export default function Page() {
@@ -15,7 +15,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape}></div>
           <p
-            className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lustina.className} `}
+            className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} `}
           >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
